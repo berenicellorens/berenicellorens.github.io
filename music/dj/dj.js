879604b4829b91c945c999391dab5b19d1170a88
berenice llorens <68783723+berenicellorens@users.noreply.github.com>
2022-08-16 12:31:59 +0200

    Update dj.js

--- music/dj/dj.js
+++ music/dj/dj.js
@@ -1,4 +1,4 @@
-import { mixcloud } from '/scripts/data.js';
+import { soundcloud, youtube } from '/scripts/data.js';
 import { loadCSV } from '/scripts/loadcsv.js'
 import { footerLinks } from '/scripts/footer.js';
 
