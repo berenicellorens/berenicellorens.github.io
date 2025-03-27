4b7689559acc8a5a81693c791609836dba841fe1
Berenice Llorens <berenicellorens@gmail.com>
2025-03-27 11:50:14 +0100

    boton nuevo intro

--- scripts/backbutton.js
+++ scripts/backbutton.js
@@ -1,15 +1,34 @@
+// import { lang } from './language.js';
+
+// const backButtonText = ['Volver', 'intro'];
+
+// export function backButton() {
+
+//   const backdiv = document.createElement('div');
+//   const backtext = document.createElement('div');
+//   backdiv.className = 'back';
+  
+//   backdiv.onclick = () => history.back();
+ 
+//   backtext.appendChild(document.createTextNode(backButtonText[lang]));
+//   backdiv.appendChild(backtext)
+//   document.querySelector('body').appendChild(backdiv);
+
+// }
+
+
 import { lang } from './language.js';
 
-const backButtonText = ['Volver', 'Back'];
+const backButtonText = ['Inicio', 'Intro'];
 
 export function backButton() {
-
   const backdiv = document.createElement('div');
   const backtext = document.createElement('div');
   backdiv.className = 'back';
-  backdiv.onclick = () => history.back();
-  backtext.appendChild(document.createTextNode(backButtonText[lang]));
-  backdiv.appendChild(backtext)
-  document.querySelector('body').appendChild(backdiv);
 
+  backdiv.onclick = () => window.location.href = '/'; // Redirige a la página de inicio
+  
+  backtext.appendChild(document.createTextNode(backButtonText[lang]));
+  backdiv.appendChild(backtext);
+  document.querySelector('body').appendChild(backdiv);
 }
