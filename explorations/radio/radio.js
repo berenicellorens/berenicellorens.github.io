176a46a735ed076bb97e33a6e6e83b161090e1e5
Berenice Llorens <berenicellorens@gmail.com>
2025-03-31 15:47:36 +0200

    limpiar exploration

--- explorations/radio/radio.js
+++
@@ -1,7 +0,0 @@
-import { soundcloud, youtube } from '/scripts/data.js';
-import { loadCSV } from '/scripts/loadcsv.js'
-import { footerLinks } from '/scripts/footer.js';
-
-loadCSV('radio.csv');
-
-footerLinks([soundcloud, youtube], 'btn');
