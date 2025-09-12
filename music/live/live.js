0d2749c96ed4aff5a8e3f5faebe25673f9dfda8d
berenice llorens <berenicellorens@gmail.com>
2025-09-12 12:01:45 +0200

    dj set live impro

---
+++ music/live/live.js
@@ -0,0 +1,7 @@
+import { soundcloud, youtube } from '/scripts/data.js';
+import { loadCSV } from '/scripts/loadcsv.js'
+import { footerLinks } from '/scripts/footer.js';
+
+loadCSV('dj.csv');
+
+footerLinks([soundcloud, youtube], 'btn');
