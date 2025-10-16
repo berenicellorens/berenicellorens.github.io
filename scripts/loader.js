b42ca4254af518f4a026041e340c8242751bffbc
Sancaf <shancaf91@gmail.com>
2025-10-16 13:59:04 -0300

    feat: try to solve some big issues and make home page prettier

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,30 +1,32 @@
-import { iframeMaxWidth } from './data.js';
+import { iframeMaxWidth } from './data.js'
 
 export async function loader(response) {
-  let rows = response.split("\n");
+  let rows = response.split('\n')
   // console.log("Load", rows);
   rows.forEach((row, index) => {
     if (index > 0) {
-
-      const cols = row.split(",");
+      const cols = row.split(',')
       // console.log("Cols", index, cols);
       // ignore empty lines
       if (cols[0] !== undefined && cols[1] !== undefined) {
-        const title = cols[0];
-        const iframe = cols[1].replaceAll("\"", "");
-        const art = document.createElement('article');
-        const wrapper = document.createElement('div');
-        const tit = document.createElement('h3');
-        tit.innerHTML = title;
-        wrapper.className = 'iframeWrapper';
-        wrapper.appendChild(tit);
-        wrapper.insertAdjacentHTML('beforeend', iframe);
-        art.appendChild(wrapper);
-        art.setAttribute('max-width', iframeMaxWidth + 'px');
-        document.querySelector('main').appendChild(art);
+        const title = cols[0]
+        const iframe = cols[1].replaceAll('"', '')
+        const art = document.createElement('article')
+        const wrapper = document.createElement('div')
+        const tit = document.createElement('h3')
+        tit.innerHTML = title
+        wrapper.className = 'iframeWrapper'
+        wrapper.appendChild(tit)
+        wrapper.insertAdjacentHTML('beforeend', iframe)
+        art.appendChild(wrapper)
+        art.setAttribute('max-width', iframeMaxWidth + 'px')
+        document.querySelector('.test').appendChild(art)
       }
     }
-  });
-  const iframes = document.querySelectorAll("iframe");
-  iframes.forEach(f => {f.width = iframeMaxWidth; f.height = iframeMaxWidth;});
+  })
+  const iframes = document.querySelectorAll('iframe')
+  iframes.forEach((f) => {
+    f.width = iframeMaxWidth
+    f.height = iframeMaxWidth
+  })
 }
